91f2d25c8db001cd80e5e34161e2bd34cd714ee4
Rizki Awal Ramadhani <rizkiawal16@gmail.com>
2022-08-24 10:48:40 +0000

    Update 2 files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
+
+const heading = React.createElement('h1, null, Biodata Perusahaan')
+
+
+
+const root = React.createElement('h1, null', 'Biodata Perusahaan')
+root.render(heading)
